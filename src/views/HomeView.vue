6da31dfc00b81e81887efd38e79c343c2a4f964d
Adrian GS <adgarcia@petroil.com.mx>
2022-08-14 18:52:27 -0600

    Segunda preba del commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,6 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <button class="btn btn-primary">primary</button>
     <button class="btn btn-secondary">secondary</button>
-    <button class="btn btn-success">success</button>
+    <button class="btn btn-success">danger</button>
   </div>
 </template>
